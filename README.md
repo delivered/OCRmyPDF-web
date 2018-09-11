b455293e6f4b80a23eca0b74e8a5a55fb4b71854
Marty Mavis <marty.mavis@gmail.com>
2018-09-11 15:44:17 -0500

    basic script working

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # OCRmyPDF-web
 
-A simple web service for running [OCRmyPDF](https://github.com/jbarlow83/OCRmyPDF). Builds on top of the official OCRmyPDF docker container and adds a simple REST API and lightweight web frontend. Simply drag a PDF file into the browser window and get a file download with the OCRed file back!
+A simple web service for running [OCRmyPDF](https://github.com/jbarlow83/OCRmyPDF). Builds on top of the official OCRmyPDF docker container and adds a simple REST API.
 
 ## Running
 
@@ -11,5 +11,5 @@ OCRmyPDF-web has been designed with [Dokku](http://dokku.viewdocs.io/dokku/) in 
 Expose on port 8888:
 
 ```bash
-docker run -p 8888:8000 --name ocrmypdf-web sseemayer/ocrmypdf-web
+docker run -p 8888:8000 --name ocrmypdf-web delivered/ocrmypdf-web
 ```
--- server.py
+++ server.py
@@ -1,43 +1,121 @@
 #!/usr/bin/env python
+
+import requests, zipfile, io, img2pdf, sys, os, time, logging, uuid, shutil
+
 import hug
 from hug_middleware_cors import CORSMiddleware
 
 import subprocess
 from tempfile import NamedTemporaryFile
 
+from datetime import datetime, timedelta
+
+from azure.storage.blob import (
+    BlockBlobService,
+    ContainerPermissions,
+    BlobPermissions,
+    PublicAccess,
+    ContentSettings)
+from applicationinsights import TelemetryClient
+
 api = hug.API(__name__)
 api.http.add_middleware(CORSMiddleware(api))
 
+# create logger
+logger = logging.getLogger('debuglogging')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 @hug.get('/', output=hug.output_format.file)
 def index():
     return "index.htm"
 
-
 @hug.get('/static/{fn}', output=hug.output_format.file)
 def static(fn):
     return 'static/{}'.format(fn)
 
-
-@hug.post('/ocr', output=hug.output_format.file)
+@hug.post('/ocr')
 def ocr(body, response, language: "The language(s) to use for OCR"="eng"):
-    if not len(body) == 1:
-        raise Exception("Need exactly one file!")
 
-    fn, content = list(body.items()).pop()
+    logger.info(os.environ)
 
-    f_out = NamedTemporaryFile(suffix='.pdf')
+    azureAccountName = os.environ['SCANOCR_STORAGE_ACCOUNT_NAME'] 
+    azureAccountKey = os.environ['SCANOCR_STORAGE_ACCOUNT_KEY']
+    appInsightsTelemetryKey = os.environ['SCANOCR_APP_INSIGHTS_TELEMETRY_KEY'] 
+    
+    fileSetId = body["file_set_id"]
+    zipFileUrl = body["zip_file_url"]
+    fileName = body["file_name"]
 
-    with NamedTemporaryFile(suffix='.pdf', mode="wb") as f_in:
-        f_in.write(content)
-        f_in.flush()
+    # initiate app insights
+    tc = TelemetryClient(appInsightsTelemetryKey)
+    tc.context.operation.id = str(uuid.uuid4())
+    tc.track_event('ocr', { 'zip_file_url': zipFileUrl, 'file_set_id': fileSetId })
+    tc.flush()
 
-        proc = subprocess.Popen(['ocrmypdf', '--force-ocr', '-l', language, f_in.name, f_out.name])
+    # download zip, extract
+    zipRequestBody = requests.get(zipFileUrl)
+    z = zipfile.ZipFile(io.BytesIO(zipRequestBody.content))
+    tempDir = '/tmp/' + fileSetId + '/' + fileName
+    if(os.path.isdir(tempDir)):
+        shutil.rmtree(tempDir)
+    z.extractall(tempDir)
 
-        code = proc.wait()
+    # grab all PNG images from zip extract results
+    image_files = []
+    for root, dirs, files in os.walk(tempDir):
+        for file in files:
+            if file.endswith(".png"):
+                image_files.append(os.path.join(root, file))
 
-        response.set_header('X-OCR-Exit-Code', str(code))
+    # log file count to app insights
+    tc.track_event('ocr_zip_extracted', { 'file_count': len(image_files) })
+    tc.flush()
 
-        print(f_out.name)
+    with open(tempDir + '/output.pdf', 'w+') as output:
 
-        return f_out
+        # convert PNGs to (non-OCR) PDF
+        pdf_bytes = img2pdf.convert(image_files)
+        file = open(tempDir + '/input.pdf',"wb")
+        file.write(pdf_bytes)
+        
+        # log progress to app insights
+        tc.track_event('ocr_pdf_created')
+        tc.flush()
+        
+        # launch OCR process
+        proc = subprocess.Popen( "ocrmypdf --jobs 4 --output-type pdf " + tempDir + "/input.pdf " + tempDir + "/output.pdf", stdin=subprocess.PIPE, shell=True )
+
+    # wait for OCR completion
+    code = proc.wait()
+
+    # log OCR completion to app insights
+    tc.track_event('ocr_output_pdf_complete')
+    tc.flush()
+
+    # upload resulting PDF to Azure
+    blob_service = BlockBlobService(account_name=azureAccountName, account_key=azureAccountKey)
+    blob_service.create_blob_from_path(
+        'images/' + fileSetId,
+        fileName + ".pdf",
+        tempDir + '/output.pdf',
+        content_settings=ContentSettings(content_type='application/pdf')
+    )
+
+    # log upload completion to app insights
+    tc.track_event('ocr_uploaded_to_azure')
+    tc.flush()
+
+    # obtain download signature from Azure
+    sas_url = blob_service.generate_blob_shared_access_signature('images/' + fileSetId,
+        fileName + "pdf",
+        BlobPermissions.READ,datetime.utcnow() + timedelta(hours=12))
+
+    download_url = 'https://'+ azureAccountName +'.blob.core.windows.net/'+ 'images/' + fileSetId +'/'+ fileName + ".pdf" +'?'+sas_url
+
+    # return results
+    return {'filename': zipFileUrl, 'files': z.namelist(), 'download_url': download_url }
